2d52a6ae43ab4abf337ce0627598fb1cba5a3916
leeyg <leeyg_dev@163.com>
2021-05-20 11:35:14 +0800

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,28 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/about/:id',
+    name: 'About',
+    props: true, // 开启 props, 会把 URL 中的参数传递给组件 在组件中通过 props 来接受 URL 参数
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  }
+]
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
---
+++ src/views/About.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="about">
+    <h1>This is an about page: {{$route.params.id}}</h1>
+    <br>
+    <h1>通过开启 props 获取： {{id}}</h1>
+  </div>
+</template>
+<script>
+
+export default {
+  name: 'About',
+  props: ['id']
+}
+</script>
