067220a78d7c05bb6b64249ebdf5719ff3161c29
liyongguo <leeyg_dev@163.com>
2021-05-20 06:14:31 +0000

    Deploying to gh-pages from @ liyongguo/vue-demo@834fdec2c883c22f4f7d8dce51140d8d9502ec06 🚀

---
+++ js/about.d4656827.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(s,t,a){"use strict";a.r(t);var e=function(){var s=this,t=s.$createElement,a=s._self._c||t;return a("div",{staticClass:"about"},[a("h1",[s._v("This is an about page: "+s._s(s.$route.params.id))]),a("br"),a("h1",[s._v("通过开启 props 获取： "+s._s(s.id))])])},n=[],o={name:"About",props:["id"]},u=o,i=a("2877"),r=Object(i["a"])(u,e,n,!1,null,null,null);t["default"]=r.exports}}]);
+//# sourceMappingURL=about.d4656827.js.map
